de377c1468573d81c0f727c03a7d1ac9d062b7f1
Yang Qiwen <yangqiwen@pachiratech.com>
2019-06-26 16:11:14 +0800

    add redis-learning

---
+++ redis-learning/src/main/java/com/yqw/redis/TestRedis.java
@@ -0,0 +1,124 @@
+package com.yqw.redis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Redis Java接口测试
+ */
+public class TestRedis {
+    private Jedis jedis;
+
+    public static void main(String[] args) {
+        TestRedis redis = new TestRedis();
+        redis.init();
+        redis.testString();
+        redis.testMap();
+        redis.testList();
+        redis.testSet();
+    }
+
+    public void init() {
+        jedis = new Jedis("127.0.0.1", 6379);
+        jedis.auth("coshaho");
+    }
+
+    public void testString() {
+        // 设置值
+        jedis.del("string1");
+        jedis.set("string1", "coshaho");
+        System.out.println(jedis.get("string1"));
+
+        // 字符串拼接
+        jedis.append("string1", " is my name"); //拼接
+        System.out.println(jedis.get("string1"));
+
+        // 删除字符串
+        jedis.del("string1");
+        System.out.println(jedis.get("string1"));
+
+        // 设置多个键值对
+        jedis.del("name");
+        jedis.del("age");
+        jedis.mset("name", "coshaho", "age", "28");
+        // 加1操作
+        jedis.incr("age");
+        System.out.println(jedis.get("name") + "-" + jedis.get("age"));
+    }
+
+    public void testMap() {
+        jedis.del("user");
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "coshaho");
+        map.put("age", "28");
+        map.put("sex", "male");
+        jedis.hmset("user", map);
+
+        // 参数1 key 参数2 filed 参数3 field
+        List<String> rsmap = jedis.hmget("user", "name", "age", "sex");
+        System.out.println(rsmap);
+
+        // 删除map中的某个键值  
+        jedis.hdel("user", "age");
+        System.out.println(jedis.hmget("user", "age"));
+        System.out.println(jedis.hlen("user"));
+        System.out.println(jedis.exists("user"));
+        System.out.println(jedis.hkeys("user"));
+        System.out.println(jedis.hvals("user"));
+
+        Iterator<String> iter = jedis.hkeys("user").iterator();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            System.out.println(key + ":" + jedis.hmget("user", key));
+        }
+    }
+
+    public void testList() {
+        // 删除list
+        jedis.del("language");
+        System.out.println(jedis.lrange("language", 0, -1));
+
+        jedis.lpush("language", "english");
+        jedis.lpush("language", "chinese");
+        jedis.lpush("language", "japanese");
+
+        // 第一个是key，第二个是起始位置，第三个是结束位置，jedis.llen获取长度 -1表示取得所有  
+        System.out.println(jedis.lrange("language", 0, -1));
+
+        jedis.del("language");
+        jedis.rpush("language", "java");
+        jedis.rpush("language", "c");
+        jedis.rpush("language", "python");
+        System.out.println(jedis.lrange("language", 0, -1));
+
+        // 双向列表，jedis 排序 
+        jedis.del("a");
+        jedis.rpush("a", "1");
+        jedis.lpush("a", "6");
+        jedis.lpush("a", "3");
+        jedis.lpush("a", "9");
+        System.out.println(jedis.lrange("a", 0, -1));
+        System.out.println(jedis.sort("a"));
+        System.out.println(jedis.lrange("a", 0, -1));
+    }
+
+    public void testSet() {
+        jedis.del("b");
+        jedis.sadd("b", "张三");
+        jedis.sadd("b", "李四");
+        jedis.sadd("b", "王五");
+        jedis.sadd("b", "胜七");
+        jedis.sadd("b", "段九");
+
+        // 移除
+        jedis.srem("b", "胜七");
+        System.out.println(jedis.smembers("b"));
+        System.out.println(jedis.sismember("b", "胜七"));
+        System.out.println(jedis.srandmember("b"));
+        System.out.println(jedis.scard("b"));
+    }
+}
--- spring-boot-learning/spring-boot-web-jsp/src/main/java/com/yqw/boot/web/jsp/Application.java
+++ spring-boot-learning/spring-boot-web-jsp/src/main/java/com/yqw/boot/web/jsp/Application.java
@@ -1,12 +1,14 @@
 package com.yqw.boot.web.jsp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * 需要配置idea的run workspace到该module才可访问到jsp页面
+ * 需要配置idea的run workspace到该module才可访问到jsp页面.
+ * 如果打成war包的话，则需要继承SpringBootServletInitializer，否则不需要.
  * Created by iQiwen on 2019/5/17.
  */
 @SpringBootApplication
