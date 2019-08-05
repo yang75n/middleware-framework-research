9b987ac90769d2a68dfc699ab8771fe8d820cbad
Yang Qiwen <yangqiwen@pachiratech.com>
2019-08-05 20:14:47 +0800

    整理中间件项目目录

---
+++ spring-boot-learning/spring-boot-rabbitmq/src/main/java/com/yqw/spring/boot/rabbitmq/customer/Customer1.java
@@ -0,0 +1,16 @@
+package com.yqw.spring.boot.rabbitmq.customer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "itcast")
+public class Customer1 {
+
+    @RabbitHandler
+    public void getMsg(String msg){
+        System.out.println("itcast："+msg);
+    }
+
+}
---
+++ spring-boot-learning/spring-boot-rabbitmq/src/main/java/com/yqw/spring/boot/rabbitmq/customer/Customer2.java
@@ -0,0 +1,16 @@
+package com.yqw.spring.boot.rabbitmq.customer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "itheima")
+public class Customer2 {
+
+    @RabbitHandler
+    public void getMsg(String msg){
+        System.out.println("itheima："+msg);
+    }
+
+}
