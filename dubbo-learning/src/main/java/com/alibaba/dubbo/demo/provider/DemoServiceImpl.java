5714e60c55d039283e14f240b5cfd088d8bcdd2b
Yang Qiwen <yangqiwen@pachiratech.com>
2019-08-14 18:12:46 +0800

    add  dubbo

--- dubbo-learning/src/main/java/com/alibaba/dubbo/demo/provider/DemoServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.dubbo.demo.provider;
-import com.alibaba.dubbo.demo.DemoService;
-
-public class DemoServiceImpl implements DemoService {
-    public String sayHello(String name) {
-    	System.out.println("called...");
-        return "Hello " + name;
-    }
-}
