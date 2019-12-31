dc7ccc24996ff41ed7d9162869ba5365e7a9ef88
Yang Qiwen <yangqiwen@pachiratech.com>
2019-12-31 17:54:16 +0800

    add some spring cloud module

---
+++ spring-cloud-learning/spring-cloud-eureka/src/main/java/com/yqw/cloud/discovery/Application.java
@@ -0,0 +1,27 @@
+package com.yqw.cloud.discovery;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Service Discovery服务器
+ * <p/>
+ * Created by iQiwen on 2019/5/15.
+ *
+ * @since 1.0.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    private static final Log logger = LogFactory.getLog(Application.class);
+
+    public static void main(String[] args) {
+        if (logger.isInfoEnabled()) {
+            logger.info("开启服务发现");
+        }
+        SpringApplication.run(Application.class, args);
+    }
+}
